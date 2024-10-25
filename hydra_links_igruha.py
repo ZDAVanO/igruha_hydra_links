37321ca90b18d6fc9ab259bcf13e19d3127f03f8
Oleh <80891624+ZeDarkAdam@users.noreply.github.com>
2024-10-25 23:16:12 +0300

    add generated json

--- hydra_links_igruha.py
+++ hydra_links_igruha.py
@@ -25,7 +25,7 @@ def format_size(size_in_bytes):
     else:
         return f"{size_in_bytes} bytes"  # Якщо менше 1 МБ
 
-
+no_date_in_torrent = 0
 
 def make_magnet_from_bytes(torrent_bytes):
     try:
@@ -52,8 +52,6 @@ def make_magnet_from_bytes(torrent_bytes):
         # Створення магнет-посилання
         magnet_link = f'magnet:?xt=urn:btih:{b32hash}&dn={name}'
         
-        
-
         # Додавання URL для анонсу
         if b'announce' in metadata:
             # print(metadata[b'announce'])
@@ -65,33 +63,29 @@ def make_magnet_from_bytes(torrent_bytes):
                 magnet_link += f'&tr={announce_list.decode()}'
             elif isinstance(announce_list, list) and announce_list:
                 magnet_link += f'&tr={announce_list[0].decode()}'
+        # else:
+        #     print('Announce Not Available in Torrent File')
 
         # Додавання загальної довжини
         magnet_link += f'&xl={total_length}'
         
-        # print(format_size(total_length))
-
         formatted_date = None
 
         # Виведення дати створення торрент-файлу, якщо вона є
         if b'creation date' in metadata:
             creation_date = metadata[b'creation date']
-            # Конвертуємо Unix timestamp в зручний формат
             formatted_date = datetime.fromtimestamp(creation_date).strftime('%Y-%m-%dT%H:%M:%SZ')
-            
-            # print(f'Creation Date: {formatted_date}')
         else:
-            print('Creation Date Not Available')
+            print('Date Not Available in Torrent File')
+            no_date_in_torrent += 1
 
-        # return magnet_link, format_size(total_length), formatted_date
-        return magnet_link, formatted_date
+        # return magnet_link, formatted_date
+        return magnet_link, formatted_date, total_length
 
     except:
         # У випадку будь-якої помилки повертаємо None
-        print('DEAD TORRENT')
-        return None, None
-
-
+        print('DEAD_TORRENT')
+        return None, None, None
 
 
 
@@ -125,16 +119,12 @@ def extract_size_and_info(size_text):
         return "N/A", "NO_INFO"  # Якщо не знайдено відповідності
 
 
-def get_download_options(url):
-    # Отримуємо HTML сторінки
-    page_response = requests.get(url)
-    soup = BeautifulSoup(page_response.text, 'html.parser')
+def get_download_options(soup):
+    # Список для збереження результатів
+    torrent_info_list = []
 
     # Шукаємо всі посилання з класом 'torrent'
     torrent_links = soup.find_all('a', class_='torrent')
-
-    # Список для збереження результатів
-    torrent_info_list = []
 
     for torrent in torrent_links:
 
@@ -146,22 +136,20 @@ def get_download_options(url):
             download_page_link = soup_2.find('a', class_='torrent2')
 
 
-
         # Шукаємо ul з id 'navbartor' всередині батьківських елементів
         navbartor = torrent.find_parent('ul', id='navbartor')
-
         if navbartor:
+
             # Шукаємо найближчий попередній елемент <center>
             center_tag = navbartor.find_previous('center')
-
             if center_tag:
+
                 # Витягуємо текст з <span> всередині <center>
                 size_and_info = center_tag.find('span', style="font-size:14pt;")
 
                 if size_and_info and download_page_link:
 
-                    size, name_details = extract_size_and_info(size_and_info.get_text(strip=True))
-
+                    site_size, name_details = extract_size_and_info(size_and_info.get_text(strip=True))
 
                     # Завантаження торрент файлу
                     torrent_url = download_page_link['href']
@@ -175,33 +163,21 @@ def get_download_options(url):
 
                         # Отримання магнет-посилання
                         # magnet_link, t_size, t_date = make_magnet_from_bytes(torrent_bytes.getvalue())
-                        magnet_link, t_date = make_magnet_from_bytes(torrent_bytes.getvalue())
+                        magnet_link, torrent_date, torrent_size = make_magnet_from_bytes(torrent_bytes.getvalue())
 
                     except requests.RequestException as e:
                         print(f"Не вдалося завантажити {torrent_url}: {e}")
-                        magnet_link, t_date = None, None  # Встановлюємо значення None, якщо не вдалося завантажити
+                        magnet_link, torrent_date, torrent_size = None, None, None  # Встановлюємо значення None, якщо не вдалося завантажити
 
                     if magnet_link:
                         # Додаємо інформацію про торрент до списку
-
-                        # torrent_info_list.append({
-                        #     'link': torrent['href'],  # Силка на торрент
-                        #     'info': name_details,  # Текст розміру
-                        #     'size': size,  # Текст розміру
-                            
-                        #     'date': t_date,  # Дата створення
-                            
-                        #     'torrent_link': torrent_url,  # Силка на торрент
-                        #     'magnet_link': magnet_link  # Магнет-посилання
-                        # })
-
                         torrent_info_list.append({
                             'info': name_details,  # Текст розміру
-                            'size': size,  # Текст розміру     
-                            'date': t_date,  # Дата створення
+                            'size': site_size,  # Текст розміру     
+                            'date': torrent_date,  # Дата створення
                             'magnet_link': magnet_link  # Магнет-посилання
                         })
-                
+
     return torrent_info_list
 
 
@@ -218,49 +194,30 @@ def get_download_options(url):
 
 
 
-
-
-
-
-
-
-
-
-
-def extract_info_from_page(url):
-    page_response = requests.get(url)
-    soup = BeautifulSoup(page_response.text, 'html.parser')
+def extract_info_from_page(soup):
 
     # Витягування дати та часу
-    date_time = None
+    site_update_date = None
     article_info = soup.find('div', {'id': 'article-film-full-info'})
     if article_info:
         time_element = article_info.find('time', {'class': 'published'})
         if time_element:
-            # date_time = time_element.text
+
+            # site_update_date = time_element.text
 
             parsed_date = datetime.strptime(time_element.text, "%d.%m.%Y, %H:%M")
             iso_format_date = parsed_date.strftime("%Y-%m-%dT%H:%M:%SZ")
-            date_time = iso_format_date    
+            site_update_date = iso_format_date
     
     # Витягування назви
-    title = None
+    site_game_title = None
     module_title_div = soup.find('div', class_='module-title')
     if module_title_div:
         h1_tag = module_title_div.find('h1')
         if h1_tag:
-            title = h1_tag.text.strip()
+            site_game_title = h1_tag.text.strip()
 
-    return date_time, title
-
-
-
-
-
-
-
-
-
+    return site_update_date, site_game_title
 
 
 
@@ -287,17 +244,8 @@ root = ET.fromstring(sitemap_content)
 namespaces = {'ns': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
 
 urls = [elem.text for elem in root.findall('.//ns:loc', namespaces)]
-urls = urls[4000:5500]
-
-
-
-
-
-
-
-
-
-
+# urls = urls[4000:4010]
+print(f"\nTotal URLs: {len(urls)}")
 
 
 # Структура для збереження даних
@@ -306,48 +254,54 @@ data = {
     "downloads": []
 }
 
+no_download_options = 0
+
 # Пройдемо по кожному URL та дістанемо дані
 for index, url in enumerate(urls, start=1):
 
-    # print(f"Processing URL : {url}")
-    date_time, game_name = extract_info_from_page(url)
+    page_response = requests.get(url)
+    soup = BeautifulSoup(page_response.text, 'html.parser')
 
-    if not date_time or not game_name:
+    site_update_date, site_game_name = extract_info_from_page(soup)
+
+    if not site_update_date or not site_game_name:
         print(f'{index}. INVALID PAGE: {url}')  
     else:
-        print(f'{index}. {date_time} | {game_name} | {url}')
-        download_options = get_download_options(url)
+        print(f'{index}. {site_game_name} / {site_update_date} / {url}')
+        download_options = get_download_options(soup)
 
         if (not download_options):
             print("    No download options")
+            no_download_options += 1
         else:
             for download_option in download_options:
 
-                title = f"{game_name} {download_option['info']}"
-
-                print(f'    {title} ({download_option['size']})')
+                title = f"{site_game_name} {download_option['info']}"
 
                 if (download_option['date']):
-                    t_date = download_option['date']
+                    uploadDate = download_option['date']
                 else:
-                    t_date = date_time
-
+                    uploadDate = site_update_date
+                
+                print(f'    {title} / {uploadDate} / {download_option['size']}')
 
                 download_info = {
                     "title": title,  # Назва файлу, припустимо, остання частина URL
                     "uris": [download_option['magnet_link']],
-                    "uploadDate": t_date,
+                    "uploadDate": uploadDate,
                     "fileSize": download_option['size'],  # Тут можна додати функцію для витягнення розміру файлу
                 }
                 data["downloads"].append(download_info)
 
 
+print(f"\nTotal URLs: {len(urls)}")
+print(f"Total Invalid Pages: {len(urls) - len(data['downloads'])}")
+print(f"Total Pages with no download options: {no_download_options}")
+print(f"Total Torrents with no date: {no_date_in_torrent}")
 
 
 
-
-# Ensure the 'jsons' directory exists
-json_dir = 'jsons'
+json_dir = 'json'
 os.makedirs(json_dir, exist_ok=True)  # Create the directory if it doesn't exist
 
 current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -361,14 +315,14 @@ with open(file_path, 'w', encoding='utf-8') as f:
 print()
 print(f"The data is saved in file {file_path}")
 
-# input("Press Enter to exit...")
 
 
 
 
-# Приклад використання
-# url = "https://itorrents-igruha.org/2564-train-station-simulator.html"
-# torrent_data = get_download_options(url)
+# url = "https://itorrents-igruha.org/8095-believe.html"
+# page_response = requests.get(url)
+# soup = BeautifulSoup(page_response.text, 'html.parser')
+# torrent_data = get_download_options(soup)
 
 # for data in torrent_data:
 #     # print(f"Download Torrent page: {data['link']}")
